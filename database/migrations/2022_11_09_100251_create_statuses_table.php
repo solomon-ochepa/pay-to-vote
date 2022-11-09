8cd64adf82f5f1e36141c59d2d7769cdc30be7dc
Solomon Ochepa <solomonochepa@gmail.com>
2022-11-09 15:46:09 +0100

    Create 2022_11_09_100251_create_statuses_table.php

---
+++ database/migrations/2022_11_09_100251_create_statuses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->string('name', 16)->unique();
+            $table->string('slug')->nullable();
+            $table->bigInteger('code')->unsigned()->unique();
+            $table->string('class')->nullable()->default('text-primary');
+            $table->string('icon')->nullable();
+            //
+            $table->uuid('id')->primary();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+    }
+};
