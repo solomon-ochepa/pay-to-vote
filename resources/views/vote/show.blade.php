173babdd5a00b12caa1c190bf3d614999f45a409
Solomon Ochepa <solomonochepa@gmail.com>
2022-11-09 15:45:15 +0100

    Update show.blade.php

--- resources/views/vote/show.blade.php
+++ resources/views/vote/show.blade.php
@@ -1,16 +1,3 @@
 <x-app-sidebar-layout>
-    <div class="col-md-8 col-lg-9 vstack gap-4">
-        <div class="card">
-            <div class="card-header">
-                <h4>Voting: {{ $vote->contestant->user->first_name }} {{ $vote->contestant->user->last_name }}</h4>
-            </div>
-
-            <div class="card-body">
-                <p>Votes: {{ $vote->total }}</p>
-                <p>Amount: {{ $vote->amount }}</p>
-
-                <button class="btn btn-sm btn-primary">Pay</button>
-            </div>
-        </div>
-    </div>
+    <livewire:vote.show :vote="$vote" />
 </x-app-sidebar-layout>
