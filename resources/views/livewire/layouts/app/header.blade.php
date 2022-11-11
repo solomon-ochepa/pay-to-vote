abb426f46da77b0bf7cd5ae9bb3de3474fe56255
Solomon Ochepa <solomonochepa@gmail.com>
2022-11-11 12:26:56 +0100

    Update header.blade.php

--- resources/views/livewire/layouts/app/header.blade.php
+++ resources/views/livewire/layouts/app/header.blade.php
@@ -22,17 +22,7 @@
             <!-- Main navbar START -->
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <!-- Nav Search START -->
-                <div class="nav mt-3 mt-lg-0 flex-nowrap align-items-center px-4 px-lg-0">
-                    <div class="nav-item w-100">
-                        <form class="rounded position-relative">
-                            <input disabled class="form-control ps-5 bg-light disabled" type="search"
-                                placeholder="Search..." aria-label="Search">
-                            <button
-                                class="btn bg-transparent px-2 py-0 position-absolute top-50 start-0 translate-middle-y"
-                                type="submit"><i class="bi bi-search fs-5"> </i></button>
-                        </form>
-                    </div>
-                </div>
+                <livewire:search />
 
                 <ul class="navbar-nav navbar-nav-scroll ms-auto">
                     {{-- <li class="nav-item dropdown">
