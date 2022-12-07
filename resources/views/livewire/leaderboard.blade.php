0997fb06b5b9fc25ec2d29d14f5a54ebbdd7b6b2
Solomon Ochepa <solomonochepa@gmail.com>
2022-12-07 07:24:51 +0100

    Update leaderboard.blade.php

--- resources/views/livewire/leaderboard.blade.php
+++ resources/views/livewire/leaderboard.blade.php
@@ -16,10 +16,14 @@
             <!-- Event leaderboard -->
             @forelse ($event->leaderboard->take(20) ?? [] as $contestant)
                 {{-- {{ dd($contestant) }} --}}
+                @php
+                    $url = route('event.contestant.show', ['event' => $event->slug, 'contestant' => $contestant->slug]);
+                    $voted = $contestant->voted > 1000 ? number_format($contestant->voted / 1000, 2) . 'k' : $contestant->voted;
+                @endphp
                 <div class="hstack gap-2 mb-3">
                     <!-- Avatar -->
                     <div class="avatar">
-                        <a href="#!">
+                        <a href="{{ $url }}">
                             <img class="avatar-img rounded-circle"
                                 src="{{ $contestant->firstMedia(['image', 'profile'])->getUrl() }}" alt="">
                         </a>
@@ -27,7 +31,7 @@
 
                     <!-- Name -->
                     <div class="overflow-hidden">
-                        <a class="h6 mb-0" href="#!">
+                        <a class="h6 mb-0" href="{{ $url }}">
                             {{ $contestant->first_name }} {{ $contestant->last_name }}
                         </a>
                         <p class="mb-0 small text-truncate">
@@ -36,9 +40,15 @@
                     </div>
 
                     <!-- Votes -->
-                    <a class="btn btn-primary-soft rounded-circle icon-md ms-auto" href="#">
-                        {{ $contestant->voted > 1000 ? number_format($contestant->voted / 1000, 2) . 'k' : $contestant->voted }}
-                    </a>
+                    <button class="btn btn-success-soft ms-auto" type="button" data-bs-toggle="modal"
+                        data-bs-target="#vote-modal" wire:click="modal('{{ $contestant->id }}')">
+                        <i class="fas fa-thumbs-up pe-1"></i><br />
+                        {{ $voted }}
+                    </button>
+
+                    {{-- <a class="btn btn-primary-soft rounded-circle icon-md ms-auto" href="#">
+                        {{ $voted }}
+                    </a> --}}
                 </div>
 
             @empty
