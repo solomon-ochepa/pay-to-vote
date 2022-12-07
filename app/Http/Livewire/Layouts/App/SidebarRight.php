e04c01c9911a6952a12f10111297e49d1ba7e999
Solomon Ochepa <solomonochepa@gmail.com>
2022-12-07 06:48:15 +0100

    Update SidebarRight.php

--- app/Http/Livewire/Layouts/App/SidebarRight.php
+++ app/Http/Livewire/Layouts/App/SidebarRight.php
@@ -2,19 +2,12 @@
 
 namespace App\Http\Livewire\Layouts\App;
 
-use App\Models\Event;
 use Livewire\Component;
 
 class SidebarRight extends Component
 {
-    public $event;
-
     public function render()
     {
-        $this->event = Event::where('default', 1)->first();
-
-        return view('livewire.layouts.app.sidebar-right', [
-            'event' => $this->event,
-        ]);
+        return view('livewire.layouts.app.sidebar-right');
     }
 }
