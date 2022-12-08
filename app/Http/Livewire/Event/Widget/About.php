1d128ee1328d8ec6127c73b3193277fead8c2761
Solomon Ochepa <solomonochepa@gmail.com>
2022-12-08 06:54:07 +0100

    Create About.php

---
+++ app/Http/Livewire/Event/Widget/About.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Event\Widget;
+
+use Livewire\Component;
+
+class About extends Component
+{
+    public function render()
+    {
+        return view('livewire.event.widget.about');
+    }
+}
