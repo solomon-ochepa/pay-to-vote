ca08cecdb53bf41d53662faab9be8c3d90d46b61
Solomon Ochepa <solomonochepa@gmail.com>
2023-02-08 19:55:46 +0100

    Update Edit.php

--- app/Http/Livewire/Event/Edit.php
+++ app/Http/Livewire/Event/Edit.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Livewire\Event;
 
+use App\Models\Event;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 use Plank\Mediable\HandlesMediaUploadExceptions;
+use Illuminate\Http\Request;
 
 class Edit extends Component
 {
@@ -27,7 +29,10 @@ class Edit extends Component
         'event.started_at'    => ['bail', 'required', 'date'],
         'event.ended_at'      => ['bail', 'required', 'date'],
         'event.about'         => ['bail', 'required', 'string', 'max:800'],
+        'event.min_vote'        => ['bail', 'required', 'integer', 'min:1'],
+        'event.vote_cost'       => ['bail', 'required', 'numeric', 'min:1'],
         'event.default'       => ['bail', 'nullable', 'string'],
+        'image'                 => ['bail', 'nullable', 'image', "mimes:jpg,jpeg,png,svg"],
     ];
 
     public function updated()
@@ -40,5 +45,25 @@ class Edit extends Component
         $this->validate([
             'image' => ['required', 'image', "max:{$this->max}", "mimes:jpg,jpeg,png,svg,webp"],
         ]);
+    }
+
+    public function update()
+    {
+        $user = auth()->user();
+
+        if (!$user->is_admin) {
+            return redirect()->route('event.index')->with('status', "You can't create Event!");
+        }
+
+        // Reset Default
+        $default = Event::where('default', 1)->first();
+        if ($default and $this->event->default === "on" and $default->id != $this->event->id) {
+            $default->default = 0;
+            $default->save();
+        }
+
+        $this->event->update();
+
+        return redirect(route('event.show', $this->event->slug));
     }
 }
