4c9b2d1b414ab127e0ff54ea870e9de846de0f67
Solomon Ochepa <solomonochepa@gmail.com>
2023-12-26 07:00:50 +0100

    Add files via upload

---
+++ routes/web/artisan.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+Route::middleware(['auth', 'verified', 'role:admin'])->prefix('artisan')->group(function () {
+    Route::get('/', function (Request $request) {
+        $output = new BufferedOutput();
+
+        Artisan::call(($request->command ?? 'optimize:clear').' -n', [], $output);
+        output($output->fetch());
+    });
+
+    Route::get('/migrate', function (Request $request) {
+        $output = new BufferedOutput;
+
+        Artisan::call('migrate', ['--path' => $request->path], $output);
+        output($output->fetch());
+
+        Artisan::call('module:migrate', [], $output);
+        output($output->fetch());
+    });
+
+    Route::get('/storage', function () {
+        $output = new BufferedOutput;
+
+        Artisan::call('storage:link -n', [], $output);
+        output($output->fetch());
+    });
+
+    Route::get('/sitemap', function () {
+        $output = new BufferedOutput;
+
+        Artisan::call('make:sitemap -n', [], $output);
+        output($output->fetch());
+    });
+});
+
+function output($text)
+{
+    $text = trim($text);
+    $text = str_replace("\n\r\n", '<br /><br />', $text);
+    $text = str_replace("\n\n", '<br />', $text);
+    echo $text.'<hr />';
+}
